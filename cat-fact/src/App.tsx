1941f84c710ac05a05689927ea8934896641d381
Anuska86 <91799755+Anuska86@users.noreply.github.com>
2026-02-25 11:54:36 +0100

    isLoading

--- cat-fact/src/App.tsx
+++ cat-fact/src/App.tsx
@@ -5,14 +5,19 @@ import type { CatFact } from "./types";
 
 function App() {
   const [catData, setCatData] = useState<CatFact | null>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
   const fetchFact = async () => {
+    setIsLoading(true);
+
     try {
       const response = await fetch("https://catfact.ninja/fact");
       const data: CatFact = await response.json();
       setCatData(data);
     } catch (error) {
       console.error("Error fetching the cat fact:", error);
+    } finally {
+      setIsLoading(false);
     }
   };
 
@@ -21,11 +26,17 @@ function App() {
       <h1>Daily Cat Facts</h1>
 
       <div className="fact-card">
-        <button onClick={fetchFact}>Get a New Cat Fact</button>
+        <button onClick={fetchFact} disabled={isLoading}>
+          {isLoading ? "Fetching a new cat fact..." : "Get a new cat fact"}
+        </button>
         {catData ? (
           <p className="fact-text">{catData.fact}</p>
         ) : (
-          <p>Click the button if you want to know something funny about cats</p>
+          <p>
+            {isLoading
+              ? "Finding the best cat fact..."
+              : "Click the button to get a cat fact!"}
+          </p>
         )}
       </div>
     </div>
